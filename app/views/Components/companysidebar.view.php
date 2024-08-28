e2919c92497fb23f19bf50807ab0de9faa37ee85
NIHARA W.A.S <sandarunihara56@gmail.com>
2024-08-28 20:29:14 +0530

    company dashboard

--- app/views/Components/companysidebar.view.php
+++ app/views/Components/companysidebar.view.php
@@ -1,3 +1,14 @@
-<div id="sidebar" class="w-1/4  bg-black text-white">
+<div id="sidebar" class="sidebar ">
+    <div class="option">
+        <i class="fas option-i fa-home"></i> 
+        <p>Dashboard</p>
+    </div>
+<i class="fas fa-users"></i> Students Requests
+<i class="fas fa-user-check"></i> Shortlisted Students
+<i class="fas fa-bullhorn"></i> Advertisements
+<i class="fas fa-calendar-alt"></i> Schedule
+<i class="fas fa-comments"></i> Messages
+<i class="fas fa-user"></i> Profile
+<i class="fas fa-sign-out-alt"></i> Log out
 
 </div>
