5e50db87bc16bf45e86aed35ae7f73296dc906e9
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2024-09-16 19:55:42 +0530

    fix some errors in student dashboad

--- app/views/Components/studentsidebar.view.php
+++ app/views/Components/studentsidebar.view.php
@@ -2,49 +2,48 @@
     <div class="logodiv">
         <img src="<?php echo ROOT ?>/assets/img/grad.png" height="200" width="200" class="logo" />
     </div>
+    <div id="sidebaroption" class="sidebaroption ">
         <a class="option dash" href="" >
-        <i class="fas option-i fa-home"></i>
-        <div>
-            <p>Dashboard</p>
-        </div>
-    </a>
-    <a class="option" href="">
-        <i class="fas fa-user"></i>
-        <div>
-            <p>Profile</p>
-        </div>
-    </a>
-    <a class="option" data-target="">
-        <i class="fas fa-bullhorn"></i>
-        <div>
-            <p>Advertisements</p>
-        </div>
-    </a>
-    <a class="option" data-target="">
-        <i class="fas fa-exclamation-circle"></i>
-        <div>
-            <p>Complain</p>
-        </div>
-    </a>
-    <a class="option" data-target="">
-        <i class="fas fa-chart-line"></i>
-        <div>
-            <p>Progress Report</p>
-        </div>
-    </a>
-    <a class="option" data-target="">
-        <i class="fas fa-comments"></i>
-        <div>
-            <p>Tech Talk</p>
-        </div>
-
-    </a>
-    </a>
-    <a class="option logout">
-        <i class="fas fa-sign-out-alt"></i>
-        <div>
-            <p>Log out</p>
-        </div>
-
-    </a>
+            <i class="fas option-i fa-home"></i>
+            <div>
+                <p>Dashboard</p>
+            </div>
+        </a>
+        <a class="option" href="">
+            <i class="fas fa-user"></i>
+            <div>
+                <p>Profile</p>
+            </div>
+        </a>
+        <a class="option" data-target="">
+            <i class="fas fa-bullhorn"></i>
+            <div>
+                <p>Advertisements</p>
+            </div>
+        </a>
+        <a class="option" data-target="">
+            <i class="fas fa-exclamation-circle"></i>
+            <div>
+                <p>Complain</p>
+            </div>
+        </a>
+        <a class="option" data-target="">
+            <i class="fas fa-chart-line"></i>
+            <div>
+                <p>Progress Report</p>
+            </div>
+        </a>
+        <a class="option" data-target="">
+            <i class="fas fa-comments"></i>
+            <div>
+                <p>Tech Talk</p>
+            </div>
+        </a>
+        <a class="option logout">
+            <i class="fas fa-sign-out-alt"></i>
+            <div>
+                <p>Log out</p>
+            </div>
+        </a>
+    </div>
 </div>
