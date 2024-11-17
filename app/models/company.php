61928826f9e8337737834d1e104389b4ee729b53
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-11-17 21:33:12 +0530

    Create Company model
    Load all companies as a table

--- app/models/company.php
+++ app/models/company.php
@@ -1,36 +1,50 @@
 <?php
 class company
 {
-	
 	use Model;
 
 	protected $table = 'company';
 
 	protected $allowedColumns = [
-
 		'CompanyId',
+		'Name',
+		'ContactPerson',
+		'ShortDesc',
+		'No',
+		'Lane',
+		'City',
+		'District',
 		'Password',
+		'Email',
+		'ContactNum',
+		'Website',
+		'Linkedin',
+		'AssistantId',
 	];
-	
+
 	public function validate($data)
 	{
 		$this->errors = [];
 
-		if(empty($data['UserId']))
-		{
-			$this->errors['UserId'] = "id is required";
+		if (empty($data['name'])) {
+			$this->errors['name'] = "Company name is required";
 		}
-		if(empty($data['password']))
-		{
+		if (empty($data['contact'])) {
 			$this->errors['password'] = "Password is required";
 		}
-	
-
-		if(empty($this->errors))
-		{
+		if (empty($this->errors)) {
 			return true;
 		}
 
 		return false;
 	}
+
+	public function findall(): array|bool
+	{
+		$query = "SELECT * FROM company";
+		$result = $this->query($query);
+		return $result;
+	}
+
+	
 }
