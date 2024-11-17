799ef60eb443452723db041c81423be9f86e696f
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-11-17 21:36:11 +0530

    Update ViewCompany.php

--- app/controllers/PDC_coordinator/ViewCompany.php
+++ app/controllers/PDC_coordinator/ViewCompany.php
@@ -2,9 +2,37 @@
 class ViewCompany
 {
     use Controller;
-    public function index()
-    {
-        // redirect("company-dashboard");
-        $this->view('Coordinator/Company/viewCompany');
+    public function index($id)
+    { // load the company details by ID
+
+        // $this->view('Coordinator/Company/viewCompany');
+
+        $model = new company;
+        $data = $model->findById($id);
+        if($data)
+        {
+            $companyData = [];
+
+            foreach ($data as $companydetail) {
+                $companyData[] = [
+                    'company_id' => $companydetail->CompanyId,
+                    'company_name' => $companydetail->Name,
+                    'email' => $companydetail->Email,
+                    'contact_person' => $companydetail->ContactPerson,
+                    'contact_number' => $companydetail->ContactNum,
+                    'address_no' => $companydetail->No,
+                    'address_lane' => $companydetail->Lane,
+                    'address_city' => $companydetail->City,
+                    'address_district' => $companydetail->District,
+                    'description' => $companydetail->ShortDesc,
+                    'website' => $companydetail->Website,
+                    'linkedin' => $companydetail->Linkedin
+                ];
+            }
+            $this->view('Coordinator/Company/viewCompany' , ['companyData'=> $companyData]);
+        }
+        else{
+            echo "No data found";
+        }
     }
 }
