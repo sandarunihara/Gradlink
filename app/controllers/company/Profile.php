00025c9ff842c3f90eb398fce30e2a038fbc437e
NIHARA W.A.S <sandarunihara56@gmail.com>
2024-11-20 00:16:06 +0530

    implement profile editing functionality; add new EditProfile view and styles

--- app/controllers/company/Profile.php
+++ app/controllers/company/Profile.php
@@ -3,6 +3,22 @@
 class Profile{
     use Controller;
     public function dashboard(){
-        $this-> view('Company/Profile');
+        $user = "";
+        if (isset($_SESSION['USER'])) {
+            $user = $_SESSION['USER'];
+        }
+        // print_r($user);
+        // print_r($user->CompanyId);
+        $this-> view('Company/Profile',['data' => $user]);
+    }
+
+    public function edit(){
+        $user = "";
+        if (isset($_SESSION['USER'])) {
+            $user = $_SESSION['USER'];
+        }
+        // print_r($user);
+        // print_r($user->CompanyId);
+        $this-> view('Company/EditProfile',['data' => $user]);
     }
 }
