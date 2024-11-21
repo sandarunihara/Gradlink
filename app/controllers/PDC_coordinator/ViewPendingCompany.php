d78b013256d39feb79227f64a065a369ea441482
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-11-21 16:05:35 +0530

    Update ViewPendingCompany.php
    Connect with backend

--- app/controllers/PDC_coordinator/ViewPendingCompany.php
+++ app/controllers/PDC_coordinator/ViewPendingCompany.php
@@ -5,6 +5,41 @@ class ViewPendingCompany
     public function index()
     {
         // redirect("company-dashboard");
-        $this->view('Coordinator/Company/viewPendingCompany');
+
+        $id = $_GET['id'] ?? null;
+        if($id === null)
+        {
+            echo "invalid id or missing company id";
+            return;
+        }
+
+        $model = new company;
+        $data = $model->findById($id);
+
+        if($data){
+            $companyData = [];
+
+            foreach ($data as $companydetail){
+                $companyData[] = [
+                    'company_id' => $companydetail->CompanyId,
+                    'company_name' => $companydetail->Name,
+                    'email' => $companydetail->Email,
+                    'contact_person' => $companydetail->ContactPerson,
+                    'contact_number' => $companydetail->ContactNum,
+                    'address_no' => $companydetail->No,
+                    'address_lane' => $companydetail->Lane,
+                    'address_city' => $companydetail->City,
+                    'address_district' => $companydetail->District,
+                    'description' => $companydetail->ShortDesc,
+                    'website' => $companydetail->Website,
+                    'linkedin' => $companydetail->Linkedin
+                ];
+            }
+
+            $this->view('Coordinator/Company/viewPendingCompany', ['companyData' => $companyData]);
+        }
+        else{
+            echo "No data found";
+        }
     }
 }
