af6e26185591b1943fef0eadb953713958d401fc
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-11-21 14:51:11 +0530

    Update pendingCompanyList.php
    To list all the pending companies

--- app/controllers/PDC_coordinator/pendingCompanyList.php
+++ app/controllers/PDC_coordinator/pendingCompanyList.php
@@ -4,7 +4,34 @@ class pendingCompanyList
     use Controller;
     public function index()
     {
-        // redirect("company-dashboard");
-        $this->view('Coordinator/Company/pendingCompanyList');
+        // $this->view('Coordinator/Company/pendingCompanyList');
+        $model = new company;
+        $data = $model->findAllPending();
+
+        if($data==false || empty($data)) {
+            $this->view('Coordinator/Company/pendingCompanyList');
+        }
+        else{
+            $companyData = [];
+
+            foreach($data as $companydetail) {
+                $companyData[] = [
+                    'company_id' => $companydetail->CompanyId,
+                    'company_name' => $companydetail->Name,
+                    'email' => $companydetail->Email,
+                    'contact_person' => $companydetail->ContactPerson,
+                    'contact_number' => $companydetail->ContactNum,
+                    'address_no' => $companydetail->No,
+                    'address_lane' => $companydetail->Lane,
+                    'address_city' => $companydetail->City,
+                    'address_district' => $companydetail->District,
+                    'description' => $companydetail->ShortDesc,
+                    'website' => $companydetail->Website,
+                    'linkedin' => $companydetail->Linkedin,
+                    'status' => $companydetail->Status,
+                ];
+            }
+            $this->view('Coordinator/Company/pendingCompanyList', ['companyData' => $companyData]);
+        }
     }
 }
