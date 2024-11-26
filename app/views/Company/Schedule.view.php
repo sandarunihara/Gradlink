30b1d301d95ca0d11311f919f0f2960ff966049c
NIHARA W.A.S <sandarunihara56@gmail.com>
2024-11-26 11:27:20 +0530

    Refactor interview schedule view to dynamically display schedule data and remove hardcoded entries

--- app/views/Company/Schedule.view.php
+++ app/views/Company/Schedule.view.php
@@ -13,7 +13,7 @@
 <body class="body">
     <div class="dashboard">
         <div class="side">
-            <?php $this->renderComponent("companysidebar",['hasShortlisted'=>$_SESSION['hasShortlisted'],'hasRecruited'=>$_SESSION['hasRecruited']])  ?>
+            <?php $this->renderComponent("companysidebar", ['hasShortlisted' => $_SESSION['hasShortlisted'], 'hasRecruited' => $_SESSION['hasRecruited']])  ?>
         </div>
         <div id="content">
             <div class="main">
@@ -26,66 +26,29 @@
                 <div class="sr_main">
                     <div class="sr_search">
                         <h3>Interview Schedule</h3>
-                        <div class="ss_create">
-                            <a href="../Schedule/Create">
-                                <button>
-                                    <i class="fas fa-plus"></i>
-                                    <h4>Create Schedule</h4>
-                                </button>
-                            </a>
-                        </div>
+
                     </div>
                     <div class="s_background">
-                        <div class="s_table">
-                            <div>
-                                <p>Position : <span>Software Engineering</span></p>
-                                <p>Date Period : <span>2024-09-12 to 2024-09-30</span></p>
-                                <p>Interview Availability</p>
-                                <ul>
-                                    <li>Monday: 9:00 AM to 12:00 PM</li>
-                                    <li>Tuesday: 1:00 PM to 5:00 PM</li>
-                                    <li>Wednesday: 9:00 AM to 5:00 PM</li>
-                                </ul>
-                            </div>
-                            <div class="s_delnedit">
-                                <i class="fas fa-pencil-alt"></i>
-                                <i class="fas fa-trash-alt fa-trash-alty"></i>
-                            </div>
-                        </div>
-                        <div class="s_table">
-                            <div>
-                                <p>Position : <span>Software Engineering</span></p>
-                                <p>Date Period : <span>2024-09-12 to 2024-09-30</span></p>
-                                <p>Interview Duration: <span>30-minute time slots</span></p>
-                                <p>Interview Availability</p>
-                                <ul>
-                                    <li>Monday: 9:00 AM to 12:00 PM</li>
-                                    <li>Tuesday: 1:00 PM to 5:00 PM</li>
-                                    <li>Wednesday: 9:00 AM to 5:00 PM</li>
-                                </ul>
-                            </div>
-                            <div class="s_delnedit">
-                                <i class="fas fa-pencil-alt"></i>
-                                <i class="fas fa-trash-alt fa-trash-alty"></i>
-                            </div>
-                        </div>
-                        <div class="s_table">
-                            <div>
-                                <p>Position : <span>Software Engineering</span></p>
-                                <p>Date Period : <span>2024-09-12 to 2024-09-30</span></p>
-                                <p>Interview Duration: <span>30-minute time slots</span></p>
-                                <p>Interview Availability</p>
-                                <ul>
-                                    <li>Monday: 9:00 AM to 12:00 PM</li>
-                                    <li>Tuesday: 1:00 PM to 5:00 PM</li>
-                                    <li>Wednesday: 9:00 AM to 5:00 PM</li>
-                                </ul>
-                            </div>
-                            <div class="s_delnedit">
-                                <i class="fas fa-pencil-alt"></i>
-                                <i class="fas fa-trash-alt fa-trash-alty"></i>
-                            </div>
-                        </div>
+                        <?php if (!empty($data)) : ?>
+                            <?php foreach ($data as $slot) : ?>
+                                <div class="s_table">
+                                    <div>
+                                        <p>Position : <span><?php echo $slot['Position']; ?></span></p>
+                                        <p>Student Name :<span> <?php echo $slot['StudentName']; ?></span></p>
+                                        <p>Date : <span><?php echo $slot['Date']; ?></span></p>
+                                        <p>Time : <span><?php echo $slot['StartTime']; ?> to <?php echo $slot['EndTime']; ?></span></p>
+                                    </div>
+                                    <div class="s_delnedit">
+                                        <i class="fas fa-pencil-alt"></i>
+                                        <i class="fas fa-trash-alt fa-trash-alty"></i>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <p>No Schedule available.</p>
+                        <?php endif; ?>
+
+
                     </div>
                 </div>
             </div>
@@ -95,8 +58,6 @@
     <div id="toast-container" class="toast-container"></div>
     <script>
         // toast model
-        
-
     </script>
 </body>
 
