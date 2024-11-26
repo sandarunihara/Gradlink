e58531927c31bd2d29b4f6304549252b60572cc5
NIHARA W.A.S <sandarunihara56@gmail.com>
2024-11-26 10:09:23 +0530

    Update header label from 'Send Email' to 'View' in Recruit Students table

--- app/views/Company/RecruitStudents.view.php
+++ app/views/Company/RecruitStudents.view.php
@@ -64,7 +64,7 @@
                                             <h5>Action</h5>
                                         </th>
                                         <th>
-                                            <h5>Send Email</h5>
+                                            <h5>View</h5>
                                         </th>
                                     </thead>
                                     <tbody>
