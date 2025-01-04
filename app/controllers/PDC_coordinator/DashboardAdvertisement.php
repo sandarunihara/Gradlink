eddda56dff020cda77d65629badecef32e337711
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-01-04 20:00:35 +0530

    Dashboard advertisement controller updated
    used 2 models to take details

--- app/controllers/PDC_coordinator/DashboardAdvertisement.php
+++ app/controllers/PDC_coordinator/DashboardAdvertisement.php
@@ -5,7 +5,38 @@ class DashboardAdvertisement
     public function index()
     {
         // redirect("company-dashboard");
-        $this->view('Coordinator/Advertisement/dashboardAdvertisement');
+
+        $model = new C_Advertisement;
+        $companyModel = new company;
+
+        $data = $model->find(['status' => "Active"]);
+
+        if ($data == false || empty($data)) {
+            $this->view('Coordinator/Advertisement/dashboardAdvertisement');
+        }
+        else {
+            $advertisementData = [];
+
+            foreach($data as $addetail)
+            {
+                $company = $companyModel->findById($addetail->CompanyId);
+                $companyName = (!empty($company) && isset($company[0]->Name)) ? $company[0]->Name : 'Unknown Company';
+                // $companyName = $company[0]->Name;
+
+                $advertisementData[] = [
+                    'advertisement_id' => $addetail->advertisementId,
+                    'position' => $addetail->position,
+                    'description' => $addetail->description,
+                    'interns' => $addetail->numOfInterns,
+                    'mode' => $addetail->workingMode,
+                    'start_date' => $addetail->startdate,
+                    'end_date' => $addetail->deadline,
+                    'company_name' => $companyName
+                ];
+
+            }
+            $this->view('Coordinator/Advertisement/dashboardAdvertisement', ['advertisementData' => $advertisementData]);
+
+        }
     }
 }
-
