ff08cf1214491cc6ae2a93f1284d718986ae2db1
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-11 13:58:09 +0530

    CSS

--- app/views/Coordinator/Session/dashboardSession.view.php
+++ app/views/Coordinator/Session/dashboardSession.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Students</title>
+    <title>Sessions</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Coordinator/Session/dashboardSession.css">
     <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Components/coordinatorDashboard.css">
