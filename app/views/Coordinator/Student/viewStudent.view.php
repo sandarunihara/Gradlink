dde9ec721fb717cf9c7c9a70f8f0145b6091a463
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-12 21:34:19 +0530

    Update viewStudent.view.php

--- app/views/Coordinator/Student/viewStudent.view.php
+++ app/views/Coordinator/Student/viewStudent.view.php
@@ -16,27 +16,17 @@
 
 <body>
     <div class="container">
-        <?php $this->renderComponent("coordinatorDashboard")  ?>
+        <?php $this->renderComponent("coordinatorDashboard") ?>
 
         <main class="main-content">
             <header class="header">
                 <div class="company-title">
                     <h1 name="company_name"><?= htmlspecialchars($studentData['student_name'] ?? '') ?></h1>
-                    <button class="edit-btn">&#9998;</button>
+
                 </div>
 
-                <div class="header-right">
-                    <i class="material-icons">notifications</i>
-                    <img src="<?= ROOT ?>/assets/images/profile_img.jpg" alt="">
 
-                    <div class="user-info">
-                        <span>Jonitha Cathrine</span>
-                        <small>Admin</small>
-                    </div>
-                </div>
             </header>
-
-            <?php $this->renderComponent("companyTabs") ?>
 
             <section class="company-info">
                 <?php if (!empty($studentData)): ?>
@@ -45,74 +35,67 @@
                         console.log(studentData);
                     </script>
                     <!-- <?php
-                            echo "<pre>";
-                            print_r($studentData);
-                            echo "</pre>";
-                            ?> -->
-                    <form class="company-form" id="companyForm" >
+                    echo "<pre>";
+                    print_r($studentData);
+                    echo "</pre>";
+                    ?> -->
+                    <form class="company-form" id="companyForm">
                         <div class="form-group">
-                            <label for="company-name">Company Name</label>
-                            <input type="text" id="company-name" name="company_name" value="<?= htmlspecialchars($companyData['company_name'] ?? '') ?>" readonly required>
-                            <?php if (!empty($errors['company_name'])): ?>
-                                <div style="color: red; font-size: 14px;"><?= htmlspecialchars($errors['company_name']) ?></div>
+                            <label for="company-name">Student Name</label>
+                            <input type="text" id="company-name" name="company_name"
+                                value="<?= htmlspecialchars($studentData['student_name'] ?? '') ?>" readonly required>
+                            <?php if (!empty($errors['student_name'])): ?>
+                                <div style="color: red; font-size: 14px;"><?= htmlspecialchars($errors['student_name']) ?></div>
                             <?php endif; ?>
                         </div>
                         <div class="form-group">
+                            <label for="contact-number">Registration No</label>
+                            <input type="text" id="contact-number" name="contact_number"
+                                value="<?= htmlspecialchars($studentData['student_id'] ?? '') ?>" readonly required>
+                        </div>
+                        <div class="form-group">
                             <label for="email-address">Email Address</label>
-                            <input type="email" id="email-address" name="email" value="<?= htmlspecialchars($companyData['email'] ?? '') ?>" readonly required>
+                            <input type="email" id="email-address" name="email"
+                                value="<?= htmlspecialchars($studentData['email'] ?? '') ?>" readonly required>
                         </div>
                         <div class="form-group">
-                            <label for="contact-number">Contact Number</label>
-                            <input type="text" id="contact-number" name="contact_number" value="<?= htmlspecialchars($companyData['contact_number'] ?? '') ?>" readonly required>
+                            <label for="contact-number">NIC</label>
+                            <input type="text" id="contact-number" name="contact_number"
+                                value="<?= htmlspecialchars($studentData['nic'] ?? '') ?>" readonly required>
                         </div>
                         <div class="form-group">
-                            <label for="address">Address No</label>
-                            <input type="text" id="address_no" name="address_no" value="<?= htmlspecialchars($companyData['address_no'] ?? '') ?>" readonly required>
+                            <label for="address">Degree</label>
+                            <input type="text" id="address_no" name="address_no"
+                                value="<?= htmlspecialchars($studentData['degree'] ?? '') ?>" readonly required>
                         </div>
                         <div class="form-group">
-                            <label for="address">Address Lane</label>
-                            <input type="text" id="address_lane" name="address_lane" value="<?= htmlspecialchars($companyData['address_lane'] ?? '') ?>" readonly required>
-                        </div>
-                        <div class="form-group">
-                            <label for="address">Address City</label>
-                            <input type="text" id="address_city" name="address_city" value="<?= htmlspecialchars($companyData['address_city'] ?? '') ?>" readonly required>
-                        </div>
-                        <div class="form-group">
-                            <label for="address">Address District</label>
-                            <input type="text" id="address_district" name="address_district" value="<?= htmlspecialchars($companyData['address_district'] ?? '') ?>" readonly required>
-                        </div>
-                        <div class="form-group">
-                            <label for="description">Description</label>
-                            <textarea id="description" name="description" readonly><?= htmlspecialchars($companyData['description']) ?></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="contact-person">Contact Person</label>
-                            <input type="text" id="contact-person" name="contact_person" value="<?= htmlspecialchars($companyData['contact_person']) ?>" readonly required>
-                        </div>
-                        <div class="form-group">
-                            <label for="website">Website</label>
-                            <input type="url" id="website" name="website" value="<?= htmlspecialchars($companyData['website']) ?>" readonly required>
+                            <label for="address">Contact No</label>
+                            <input type="text" id="address_lane" name="address_lane"
+                                value="<?= htmlspecialchars($studentData['contact_no'] ?? '') ?>" readonly required>
                         </div>
                         <div class="form-group">
                             <label for="linkedin">LinkedIn</label>
-                            <input type="url" id="linkedin" name="linkedin" value="<?= htmlspecialchars($companyData['linkedin']) ?>" readonly required>
+                            <input type="url" id="linkedin" name="linkedin"
+                                value="<?= htmlspecialchars($studentData['linkedin']) ?>" readonly required>
+                        </div>
+                        <div class="form-group">
+                            <label for="linkedin">Github</label>
+                            <input type="url" id="linkedin" name="linkedin"
+                                value="<?= htmlspecialchars($studentData['gitlink']) ?>" readonly required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea id="description" name="description"
+                                readonly><?= htmlspecialchars($studentData['description']) ?></textarea>
                         </div>
                         <button class="btn update-btn" id="save-btn" type="submit" style="display: none;">Update</button>
                         <div>
-                        <small id="contact-error" class="error-message" style="color: red; display: none; ">
-                            Please enter a valid contact number (10 digits, starting with 07).
-                        </small>
-                    </div>
-                    </form>
-                    <div class="button-line">
-                        <button class="view-profile-btn hidden" >LinkedIn</button>
-                        <div class="action-buttons" >
-                            <button class="btn update-btn" >View Profile</button>
-                            <!-- <button class="btn delete-btn" id="delete-btn" onclick="clickDeleteBtn('<?= $companyData[0]['company_id'] ?>');">Delete</button> -->
-                            <!-- <button class="btn update-btn" id="update-btn" onclick="enableEditing()">Edit</button> -->
-
+                            <small id="contact-error" class="error-message" style="color: red; display: none; ">
+                                Please enter a valid contact number (10 digits, starting with 07).
+                            </small>
                         </div>
-                    </div>
+                    </form>
+
                 <?php else: ?>
                     <p>No company data available.</p>
                 <?php endif; ?>
