532cb0ea071998a5b867bfd0e3af2e7e87780b3d
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-14 05:26:40 +0530

    CSS for view student profile

--- app/views/Coordinator/Student/viewStudent.view.php
+++ app/views/Coordinator/Student/viewStudent.view.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Profile</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/Coordinator/Company/viewCompany.css">
-    <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Components/companyTabs.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/Coordinator/Student/viewStudent.css">
     <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Components/coordinatorDashboard.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
