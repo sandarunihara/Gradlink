cdec2cf810a87a4fea83a81b5a04973431e8f0ec
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-17 00:03:39 +0530

    IS counts functions  and pass to the frontend

--- app/controllers/PDC_coordinator/Dashboard.php
+++ app/controllers/PDC_coordinator/Dashboard.php
@@ -18,6 +18,12 @@ class Dashboard
         $recruitedCSCount = $coordinatorModel->recruitedCSCount();
         $CSCount = $coordinatorModel->totalCSCount();
 
+        $pendingISCount = $coordinatorModel->pendingISCount();
+        $rejectedISCount = $coordinatorModel->rejectedISCount();
+        $recruitedISCount = $coordinatorModel->recruitedISCount();
+        $ISCount = $coordinatorModel->totalISCount();
+
+
         if ($totalCompanies === null || $totalStudents === null || $totalAdvertisements === null) {
             $this->view('Coordinator/Company/dashboard');
             return;
@@ -28,6 +34,11 @@ class Dashboard
             return;
         }
          
+        if ($pendingISCount === null || $rejectedISCount === null || $recruitedISCount === null || $ISCount === null) {
+            $this->view('Coordinator/Company/dashboard');
+            return;
+        }
+
             $dashboardData = [];
             $applicationGraph = [];
 
@@ -38,11 +49,16 @@ class Dashboard
             ];
 
             $notAppliedCSCount = $CSCount - ($pendingCSCount+$rejectedCSCount+$recruitedCSCount);
+            $notAppliedISCount = $ISCount - ($pendingISCount+$rejectedISCount+$recruitedISCount);
             $applicationGraph = [
                 'pendingCSCount' => (($pendingCSCount * 100)/$CSCount) ?? 0,
                 'rejectedCSCount' => (($rejectedCSCount * 100)/$CSCount) ?? 0,
                 'recruitedCSCount'=> (($recruitedCSCount * 100)/$CSCount) ?? 0,
                 'notAppliedCSCount' => (($notAppliedCSCount * 100)/$CSCount),
+                'pendingISCount' => (($pendingISCount * 100)/$ISCount) ?? 0,
+                'rejectedISCount' => (($rejectedISCount * 100)/$ISCount) ?? 0,
+                'recruitedISCount'=> (($recruitedISCount * 100)/$ISCount) ?? 0,
+                'notAppliedISCount' => (($notAppliedISCount * 100)/$ISCount),
             ];
 
             // echo '<pre>';
