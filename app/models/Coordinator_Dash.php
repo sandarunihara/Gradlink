06ae08094f2b6ffbdaebdc80c153b37e5aee6973
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-17 00:03:09 +0530

    IS counts functions with DB queries

--- app/models/Coordinator_Dash.php
+++ app/models/Coordinator_Dash.php
@@ -63,9 +63,75 @@ class Coordinator_Dash
             $result = $this->query($query);
             return $result[0]->{'CSTotal'};
         }catch (Exception $e) {
-            error_log("Error fetching total number of recruited CS students: " . $e->getMessage());
+            error_log("Error fetching total number CS students: " . $e->getMessage());
             return false;
         }
     }
+
+    public function pendingISCount(): mixed
+    {
+        try {
+            $query = "SELECT COUNT(*) AS pendingTotal
+                        FROM student
+                        JOIN studentadvertisement ON studentadvertisement.StudentId = student.StudentId
+                        WHERE studentadvertisement.jobstatus IN ('Pending', 'Shortlist') AND student.DegreeName = 'Information System'";
+
+            $result = $this->query($query);
+            return $result[0]->{'pendingTotal'};
+        } catch (Exception $e) {
+            error_log("Error fetching total number of pending IS students: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function rejectedISCount(): mixed
+    {
+        try {
+            $query = "SELECT COUNT(*) AS rejectedTotal
+                        FROM student
+                        JOIN studentadvertisement ON studentadvertisement.StudentId = student.StudentId
+                        WHERE studentadvertisement.jobstatus = 'Reject' AND student.DegreeName = 'Information System'";
+
+            $result = $this->query($query);
+            return $result[0]->{'rejectedTotal'};
+        }catch (Exception $e) {
+            error_log("Error fetching total number of rejected IS students: " . $e->getMessage());
+            return false;
+        }
+        
+    }
+
+    public function recruitedISCount(): mixed
+    {
+        try {
+            $query = "SELECT COUNT(*) AS recruitedTotal
+                        FROM student
+                        JOIN studentadvertisement ON studentadvertisement.StudentId = student.StudentId
+                        WHERE studentadvertisement.jobstatus = 'Recruit' AND student.DegreeName = 'Information System'";
+
+            $result = $this->query($query);
+            return $result[0]->{'recruitedTotal'};
+        }catch (Exception $e) {
+            error_log("Error fetching total number of recruited IS students: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function totalISCount(): mixed
+    {
+        try {
+            $query = "SELECT COUNT(*) AS ISTotal
+                        FROM student
+                        WHERE student.DegreeName = 'Information System'";
+
+            $result = $this->query($query);
+            return $result[0]->{'ISTotal'};
+        }catch (Exception $e) {
+            error_log("Error fetching total number of IS students: " . $e->getMessage());
+            return false;
+        }
+    }
+
+
 
 }
