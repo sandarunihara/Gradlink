1236c282d526af7e1784e18947463fd2912b10eb
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-22 11:56:59 +0530

    Query for count companies by status

--- app/models/Coordinator_Dash.php
+++ app/models/Coordinator_Dash.php
@@ -173,6 +173,19 @@ class Coordinator_Dash
             error_log("Error fetching job roles: " . $e->getMessage());
             return false;
         }
+    }
+
+    public function countByCompanyStatus(): mixed
+    {
+        try {
+            $query = "SELECT Status, COUNT(*) as count FROM company GROUP BY Status";
+            $result = $this->query($query);
+            return $result;
+        }
+        catch (Exception $e) {
+            error_log("Error fetching company status: " . $e->getMessage());
+            return false;
+        }
 
     }
 
