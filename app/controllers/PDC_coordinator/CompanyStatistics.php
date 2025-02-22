e7fbc4a2ddbd388d4c26341bb56f5c7781dbb2a2
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-22 11:28:34 +0530

    Controller for company locations

--- app/controllers/PDC_coordinator/CompanyStatistics.php
+++ app/controllers/PDC_coordinator/CompanyStatistics.php
@@ -4,8 +4,18 @@ class CompanyStatistics
     use Controller;
     public function index()
     {
+        $coordinatorModel = new Coordinator_Dash;
+
+        $companyLocationData = $coordinatorModel->companyLocations();
         // redirect("company-dashboard");
-        $this->view('Coordinator/Company/companyStatistics');
+        
+        if ($companyLocationData === null) {
+            $this->view('Coordinator/Company/companyStatistics');
+            return;
+
+        }
+        
+        $this->view('Coordinator/Company/companyStatistics', ['companyLocations'=> $companyLocationData]);
     }
 }
 
