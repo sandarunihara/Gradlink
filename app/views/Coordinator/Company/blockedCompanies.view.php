2a56c53fdb369d7b9a09539321e33e4e5fb1f518
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-23 20:02:08 +0530

    remove the menu

--- app/views/Coordinator/Company/blockedCompanies.view.php
+++ app/views/Coordinator/Company/blockedCompanies.view.php
@@ -20,7 +20,6 @@
         <main class="main-content">
             <header class="header">
                 <div class="header-left">
-                    <i class="material-icons">menu</i>
                     <h1>Companies</h1>
                 </div>
 
