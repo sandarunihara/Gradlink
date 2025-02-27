b34e4f55a9ff2fe75ff02d3ed070e57dab5fddbc
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-02-27 14:56:36 +0530

    Update rounds dashboard

--- app/views/Coordinator/Round/dashboardRound.view.php
+++ app/views/Coordinator/Round/dashboardRound.view.php
@@ -1,1 +1,47 @@
-sdc
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rounds</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/Coordinator/Round/dashboardRound.css">
+    <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Components/coordinatorDashboard.css">
+    <link rel="stylesheet" href="<?= ROOT ?> /assets/css/Components/companyTabs.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <?php $this->renderComponent("coordinatorDashboard") ?>
+        <main class="main-content">
+            <header class="header">
+                <div class="header-left">
+                    <h1>Rounds</h1>
+                </div>
+
+            </header>
+
+            <div class="tab-content">
+                <!-- Company List Tab -->
+                <div id="company-list" class="tab-pane active ">
+                    <!-- <section class="company-list"> -->
+                    <div class="list-header">
+                        <h2>This is Round 01</h2>
+                        
+                    </div>
+
+                </div>
+
+
+            </div>
+        </main>
+    </div>
+
+    <script src="<?= ROOT ?>/assets/js/script.js"></script>
+    
+
+</html>
