95287d185b315cdbb3e1e65854f528f42a3838ad
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-03-02 23:46:07 +0530

    Update round.php

--- app/models/round.php
+++ app/models/round.php
@@ -9,8 +9,17 @@ class round
         'roundId',
         'round',
         'active',
+        'from',
+        'to'
 	];
 
+    public function findall()
+    {
+        $query = "SELECT * FROM $this->table";
+
+        $result = $this->query($query);
+        return $result;
+    }
     public function getRound(){
         $query = "SELECT round FROM $this->table WHERE active = 1";
         $result = $this->query($query);
