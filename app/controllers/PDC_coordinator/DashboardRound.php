37bedd6b204452ec6727e9274d1ff411f824540d
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-03-02 23:47:48 +0530

    Update DashboardRound.php

--- app/controllers/PDC_coordinator/DashboardRound.php
+++ app/controllers/PDC_coordinator/DashboardRound.php
@@ -5,6 +5,27 @@ class DashboardRound
     use Controller;
     public function index()
     {
+        $model = new round;
+        $data = $model->findall();
+
+        if ($data == false || empty($data)) {
+            $this->view('Coordinator/Round/dashboardRound');
+        } else {
+            $roundData = [];
+
+            foreach ($data as $roundDetail) {
+                $roundData[] = [
+                    'round_id' => $roundDetail->roundId,
+                    'round' => $roundDetail->round,
+                    'active' => $roundDetail->active,
+                    'from' => $roundDetail->from,
+                    'to' => $roundDetail->to
+                ];
+            }
+            $this->view('Coordinator/Round/dashboardRound', ['roundData' => $roundData]);
+        }
+
+
         $this->view('Coordinator/Round/dashboardRound');
     }
 }
