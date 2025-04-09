e4f38b330387059c8cc98d8f5293e2d12c4ba58e
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-04-09 10:37:27 +0530

    Advertisements fixed

--- app/controllers/PDC_coordinator/DashboardAdvertisement.php
+++ app/controllers/PDC_coordinator/DashboardAdvertisement.php
@@ -2,45 +2,6 @@
 class DashboardAdvertisement
 {
     use Controller;
-    public function index()
-    {
-        // redirect("company-dashboard");
-
-        $model = new C_Advertisement;
-        $companyModel = new company;
-
-        $data = $model->find(['status' => "Active"]);
-
-        // echo '<pre>';
-        // print_r($data);
-        // echo '</pre>';
-
-        if ($data == false || empty($data)) {
-            $this->view('Coordinator/Advertisement/dashboardAdvertisement');
-        } else {
-            $advertisementData = [];
-
-            foreach ($data as $addetail) {
-
-                $company = $companyModel->findById($addetail->CompanyId);
-                $companyName = (!empty($company) && isset($company->Name)) ? $company->Name : 'Unknown Company';
-                // $companyName = $company[0]->Name;
-
-                $advertisementData[] = [
-                    'advertisement_id' => $addetail->advertisementId,
-                    'position' => $addetail->position,
-                    'description' => $addetail->description,
-                    'interns' => $addetail->numOfInterns,
-                    'mode' => $addetail->workingMode,
-                    'start_date' => $addetail->startdate,
-                    'end_date' => $addetail->deadline,
-                    'company_name' => $companyName
-                ];
-            }
-
-            $this->view('Coordinator/Advertisement/dashboardAdvertisement', ['advertisementData' => $advertisementData]);
-        }
-    }
 
     public function active(){
         $model = new C_Advertisement;
