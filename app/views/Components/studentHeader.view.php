ad72babe74716ec0ffbe7c062024dd42bb5fb5b5
SamarathungaSASC <140165478+SamarathungaSASC@users.noreply.github.com>
2025-04-26 21:09:15 +0530

    update student pages

--- app/views/Components/studentHeader.view.php
+++ app/views/Components/studentHeader.view.php
@@ -1,11 +1,9 @@
 <div class="header-container">
     <div class="header-left">
-    <h3><?php echo isset($title) ? htmlspecialchars($title) : 'Default Title'; ?></h3>
-    </div>
-    <div class="header-right">
-        <div class="header-icons">
-            <i class="fas fa-calendar-alt"></i>
-            <i class="fas fa-bell"></i>
+        <h1 class="header-title"><?php echo isset($title) ? htmlspecialchars($title) : 'Default Title'; ?></h1>
+        <div class="round-badge">
+            <span class="round-label">Current Round</span>
+            <span class="round-number"><?php echo isset($_SESSION['ROUNDID']) ? htmlspecialchars($_SESSION['ROUNDID']) : 'N/A'; ?></span>
         </div>
     </div>
 </div>
